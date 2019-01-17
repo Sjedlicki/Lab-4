d033247033e10ac940ec703575c3fd641dc5c4a0
stevojed <jedi1014@comcast.net>
2019-01-16 22:26:11 -0500

    Added Array

--- Lab 4/Program.cs
+++ Lab 4/Program.cs
@@ -13,26 +13,47 @@ namespace Lab_4
             bool repeat = true;
             while (repeat == true)
             {
-                Console.WriteLine("Learn your squares and cubes!\n");
+                Console.WriteLine("Learn your squares and cubes!\n");                
                 Console.Write("Enter an interger: ");
-                //int userInput = int.Parse(Console.ReadLine());
-                int userInput;
-                bool valid = false;
-                valid = int.TryParse(Console.ReadLine(), out userInput);
+
+                bool valid = int.TryParse(Console.ReadLine(), out int userInput);
+                
                 int z = Square(userInput);
                 int o = Cube(userInput);
 
-                Console.WriteLine("Number\t\tSquare\t\tCube");
+                Console.WriteLine("\nNumber\t\tSquare\t\tCube");
                 Console.WriteLine("======\t\t=======\t\t======");
 
                 for (int x = 1; x <= userInput; x++)
                 {
-                    z = Square(x);
-                    o = Cube(x);
+                z = Square(x);
+                o = Cube(x);
 
-                    Console.WriteLine(x + "\t\t" + z + "\t\t" + o);
+                Console.WriteLine(x + "\t\t" + z + "\t\t" + o);
                 }
-                    Console.Write("Thanks for Squaring and Cubing with us today!\nWould you like to enter a new value? (y/n) ");
+
+                Console.WriteLine();
+                Console.WriteLine("Your array values are: \n");
+
+                Console.WriteLine("Number\t\tSquare\t\tCube");
+                Console.WriteLine("======\t\t=======\t\t======");
+
+
+                for (int i = 0; i < userInput; i++)
+                {
+
+                int[] squared = new int[userInput];
+                int[] cubed = new int[userInput];
+                squared[i] = Square(i + 1);
+                cubed[i] = Cube(i + 1);
+
+                Console.WriteLine(i + 1 + "\t\t" + squared[i] + "\t\t" + cubed[i]);
+                //Console.WriteLine(string.Join(",", array1));
+
+                }
+                
+                Console.WriteLine();
+                Console.Write("Thanks for Squaring and Cubing with us today!\nWould you like to enter a new value? (y/n) ");
 
                 string go = Console.ReadLine().ToLower();
                 if (go == "y")
